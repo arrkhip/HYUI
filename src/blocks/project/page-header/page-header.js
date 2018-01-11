5fe9cee7469a05d67e8e5301b4f54d2eb8ee5875
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2018-01-11 14:46:44 +0300

    Добавил фиксацию page-header

--- src/blocks/project/page-header/page-header.js
+++ src/blocks/project/page-header/page-header.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
 	$('.js-page-header__switch').click(function() {
 		$(this).toggleClass('page-header__switch--active');
 		$('.page-header__list').toggleClass('page-header__list--active');
-
 	});
+
+	var nav = document.querySelector('.page-header');
+	var topOffset = 50;
+
+    window.onscroll = function() {
+      if (nav.classList.contains('page-header--fixed') && window.pageYOffset < topOffset) {
+        nav.classList.remove('page-header--fixed');
+      } else if (window.pageYOffset > topOffset) {
+        nav.classList.add('page-header--fixed');
+      }
+    };
+
 });
