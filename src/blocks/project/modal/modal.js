4509d5321bb4e3cbe05e49ba56041701143949f9
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2018-02-10 10:01:28 +0300

    modal

--- src/blocks/project/modal/modal.js
+++ src/blocks/project/modal/modal.js
@@ -3,19 +3,23 @@ $( document ).ready(function() {
     // modal dispatch
     $('.btn--open-dispatch').click(function() {
     	$('.modal--dispatch').fadeIn();
+        $('body').addClass('scroll-hidden');
     });
 
     $('.modal--dispatch .modal__head-close').click(function() {
     	$('.modal--dispatch').fadeOut();
+        $('body').removeClass('scroll-hidden');
     });
 
 
     // modal contact
     $('.btn--open-contact').click(function() {
     	$('.modal--contact').fadeIn();
+        $('body').addClass('scroll-hidden');
     });
 
     $('.modal--contact .modal__head-close').click(function() {
     	$('.modal--contact').fadeOut();
+        $('body').removeClass('scroll-hidden');
     });
 });
